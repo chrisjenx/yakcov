ef606414cac6630ffdcb7fc0d195a8bfc09c65cb
Chris Jenkins <chris.mark.jenkins@gmail.com>
2024-07-24 16:56:46 -0600

    add password example

--- sample/src/main/java/com/chrisjenx/yakcov/sample/SampleActivity.kt
+++ sample/src/main/java/com/chrisjenx/yakcov/sample/SampleActivity.kt
@@ -9,15 +9,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.chrisjenx.yakcov.Email
+import com.chrisjenx.yakcov.MinLength
+import com.chrisjenx.yakcov.PasswordMatches
 import com.chrisjenx.yakcov.Required
 import com.chrisjenx.yakcov.rememberTextFieldValueValidator
 import com.chrisjenx.yakcov.sample.ui.theme.YakcovTheme
@@ -35,7 +39,7 @@ class SampleActivity : ComponentActivity() {
                             .padding(innerPadding)
                             .padding(16.dp)
                     ) {
-
+                        Text(text = "Email", style = MaterialTheme.typography.headlineSmall)
                         val emailValidator = rememberTextFieldValueValidator(
                             rules = listOf(Required, Email), alwaysShowRule = true
                         )
@@ -52,6 +56,58 @@ class SampleActivity : ComponentActivity() {
                                     autoCorrect = false,
                                     keyboardType = KeyboardType.Email,
                                 ),
+                                singleLine = true,
+                                supportingText = supportingText()
+                            )
+                        }
+
+                        Text(text = "Password", style = MaterialTheme.typography.headlineSmall)
+                        // Password example
+                        val passwordValidator = rememberTextFieldValueValidator(
+                            rules = listOf(Required, MinLength(minLength = 6)),
+                            alwaysShowRule = true
+                        )
+                        val passwordMatchesValidator = rememberTextFieldValueValidator(
+                            rules = listOf(
+                                Required,
+                                MinLength(minLength = 6),
+                                PasswordMatches(passwordValidator)
+                            ),
+                            alwaysShowRule = true
+                        )
+
+                        with(passwordValidator) {
+                            OutlinedTextField(
+                                value = value,
+                                label = { Text(text = "Password") },
+                                modifier = Modifier
+                                    .validateFocusChanged()
+                                    .fillMaxWidth(),
+                                onValueChange = ::onValueChange,
+                                isError = isError(),
+                                keyboardOptions = KeyboardOptions(
+                                    autoCorrect = false,
+                                    keyboardType = KeyboardType.Password,
+                                ),
+                                visualTransformation = PasswordVisualTransformation(),
+                                singleLine = true,
+                                supportingText = supportingText()
+                            )
+                        }
+                        with(passwordMatchesValidator) {
+                            OutlinedTextField(
+                                value = value,
+                                label = { Text(text = "Confirm Password") },
+                                modifier = Modifier
+                                    .validateFocusChanged()
+                                    .fillMaxWidth(),
+                                onValueChange = ::onValueChange,
+                                isError = isError(),
+                                keyboardOptions = KeyboardOptions(
+                                    autoCorrect = false,
+                                    keyboardType = KeyboardType.Password,
+                                ),
+                                visualTransformation = PasswordVisualTransformation(),
                                 singleLine = true,
                                 supportingText = supportingText()
                             )
