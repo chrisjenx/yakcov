a0d1b40926bb6487d099f11581939abd41cd84c6
Chris Jenkins <chris.mark.jenkins@gmail.com>
2025-04-15 15:03:00 -0600

    add validator modifier

---
+++ library/src/commonMain/kotlin/com/chrisjenx/yakcov/Modifiers.kt
@@ -0,0 +1,23 @@
+package com.chrisjenx.yakcov
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+/**
+ * Returns a [Modifier] that will modify how the field acts to user interaction and validation
+ *
+ * @see ValueValidator.validationConfig
+ */
+@Composable
+fun Modifier.validationConfig(
+    validator: ValueValidator<*, *>,
+    validateOnFocusLost: Boolean = false,
+    shakeOnInvalid: Boolean = false,
+    showErrorOnInteraction: Boolean = true,
+): Modifier = with(validator) {
+    validationConfig(
+        validateOnFocusLost = validateOnFocusLost,
+        shakeOnInvalid = shakeOnInvalid,
+        showErrorOnInteraction = showErrorOnInteraction,
+    )
+}
