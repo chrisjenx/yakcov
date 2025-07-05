2806650a104345b8f19eeda1fce596983b54517d
Chris Jenkins <chris.mark.jenkins@gmail.com>
2025-07-05 09:25:11 -0600

    bump sample

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.chrisjenx.yakcov.sample"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.chrisjenx.yakcov.sample"
         minSdk = 23
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
